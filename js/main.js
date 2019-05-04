41fa07c383033bfaee3d6bc273142d661bb0e8c0
Syuparn <s.hello.spagetti@gmail.com>
2019-05-05 00:09:24 +0900

    first commit

---
+++ js/main.js
@@ -0,0 +1,6 @@
+var table = document.getElementById('game_table');
+if (table != null) {
+    console.log(table.rows[0]);
+    console.log(table.rows[0].cells[2]);
+    table.rows[1].cells[6].innerHTML = "s";
+}
